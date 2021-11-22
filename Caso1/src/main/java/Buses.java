3266f3e72513f4ddf62f095c829f06087a23d64e
Diegonsio <d.medina07@ufromail.cl>
2021-11-22 17:42:25 -0300

    evidencia

---
+++ Caso1/src/main/java/Buses.java
@@ -0,0 +1,5 @@
+public class Buses {
+	private Object viejo;
+	private Object nuevo;
+	public Empresa__ unnamed_Empresa_ = new Empresa[0];
+}
---
+++ Caso1/src/main/java/Empresa.java
@@ -0,0 +1,5 @@
+public class Empresa {
+	private Object buses;
+	public Empresa unnamed_Empresa_;
+	public Buses unnamed_Buses_;
+}
